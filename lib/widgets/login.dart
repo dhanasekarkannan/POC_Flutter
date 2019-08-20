d39ec015d3a463ba5fe22be892de28a1e01908fa
Dhanasekar <sendmailtodhana@gmail.com>
2019-08-20 20:12:06 +0530

    test today

--- lib/widgets/login.dart
+++ lib/widgets/login.dart
@@ -3,64 +3,8 @@ import 'package:flutter/material.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: EdgeInsets.all(15),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                  height: 220,
-                  child: Card(
-                    child: Image.asset(
-                      'assets/images/hdfc-life-logo.jpg',
-                    ),
-                    elevation: 6,
-                  )),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Welcome to HDFC Life Vendor Login',
-                style: TextStyle(color: Colors.blue[600]),
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Username'),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                ),
-                obscureText: true,
-              ),
-              RaisedButton(
-                child: Text('Login'),
-                color: Colors.red,
-                textColor: Colors.white,
-                onPressed: () {
-                  Navigator.of(context).pushReplacementNamed('/locationInput');
-                },
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  FlatButton(
-                    child: Text('Forgot Password?'),
-                    textColor: Colors.blue,
-                    onPressed: () {},
-                  ),
-                  FlatButton(
-                    child: Text('Register'),
-                    textColor: Colors.blue,
-                    onPressed: () {},
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
+    return Container(
+      
     );
   }
 }
